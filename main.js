dc748d3c2f40bc82c135deb7aa614569ce39c5ca
icedanyiel <danyielwest@gmail.com>
2020-11-11 19:37:56 +0200

    Curs 1 - Intoducere HTML,CSS, JS

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = "Daniel";
+var ammount = 100;
+var knowsJS = true;
+var fruits = ["apple", "lemon", "orange"];
+
+function add() {
+    return 2+3
+}
+
+var person = {
+    height: 2,
+    canWalk: function() {
+        console.log("can walk");
+    },
+    name: "Jon"
+}//object
