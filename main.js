b65903c4fdaf25c8f84b3086fdc46b24febd5f06
icedanyiel <danyielwest@gmail.com>
2020-11-18 16:33:40 +0200

    Tema1

--- main.js
+++ main.js
@@ -10,7 +10,17 @@ function add() {
 var person = {
     height: 2,
     canWalk: function() {
-        console.log("can walk");
+        //console.log("can walk");
     },
     name: "Jon"
 }//object
+
+//tema 1:
+var canvas = {
+    height: 600,
+    width: 400,
+    unit: "px"
+}
+console.log("height of canvas is",canvas.height,canvas.unit);
+console.log("width of canvas is",canvas.width,canvas.unit);
+
